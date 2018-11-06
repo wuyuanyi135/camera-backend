e8376db692744ac1f469b5372f0e93831c08c3be
wuyuanyi <wuyuanyi135@gmail.com>
2018-11-06 14:36:40 -0500

    working example getting camera list

--- src/main.c
+++ src/main.c
@@ -8,10 +8,20 @@
 int main() {
 
   VmbVersionInfo_t version;
-  VmbVersionQuery (&version, sizeof(version));
+  VmbVersionQuery(&version, sizeof(version));
 
-  printf ("Version = %d.%d.%d \n", version.major, version.minor, version.patch);
-  VmbError_t result = VmbStartup ();
-  printf ("result = %d\n", result);
-  getchar ();
+  printf("Version = %d.%d.%d \n", version.major, version.minor, version.patch);
+  VmbError_t result = VmbStartup();
+  printf("result = %d\n", result);
+
+  VmbCameraInfo_t cameraInfo;
+  VmbUint32_t numFound;
+  VmbCamerasList(&cameraInfo, 1, &numFound, sizeof(cameraInfo));
+
+  printf("Found %d camera(s)\n", numFound);
+
+  if (numFound)
+    printf("Camera #1: Name: %s, Id:%s", cameraInfo.cameraName, cameraInfo.cameraIdString);
+
+  getchar();
 }
